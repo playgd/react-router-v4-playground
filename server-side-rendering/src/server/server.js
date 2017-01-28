01c3598d14c2c5baf4678d7231cc09a636e04179
Fernando Daciuk <f.daciuk@gmail.com>
2017-01-28 21:06:22 -0200

    Update server.js

--- server-side-rendering/src/server/server.js
+++ server-side-rendering/src/server/server.js
@@ -1,15 +1,10 @@
 'use strict'
 
 import express from 'express'
-import React from 'react'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-if (process.env.NODE_ENV === 'development') {
-  require('./server-dev').default(app)
-} else {
-  require('./server-prod').default(app)
-}
+require('./server-app').default(app)
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
