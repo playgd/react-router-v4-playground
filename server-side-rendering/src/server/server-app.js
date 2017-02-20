7652848eb86e6d7584b222c732d5ed31affb512b
Fernando Daciuk <f.daciuk@gmail.com>
2017-02-19 22:21:27 -0300

    Update react-router api

--- server-side-rendering/src/server/server-app.js
+++ server-side-rendering/src/server/server-app.js
@@ -4,7 +4,7 @@ import path from 'path'
 import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import { ServerRouter, createServerRenderContext } from 'react-router'
+import { StaticRouter } from 'react-router'
 import App from '../app'
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -16,22 +16,26 @@ const generateHtml = htmlServer(app)
 app.use(express.static(path.join(__dirname, '..')))
 
 app.use(async (req, res) => {
-  const context = createServerRenderContext()
+  const context = {}
   const htmlApp = renderToString(
-    <ServerRouter location={req.url} context={context}>
+    <StaticRouter location={req.url} context={context}>
       <App />
-    </ServerRouter>
+    </StaticRouter>
   )
 
-  const result = context.getResult()
+  console.log('what is in context?', context)
+  console.log('req.url:', req.url)
 
-  console.log('what is in result?', result)
-
-  if (result.redirect) console.log('redirect')
-  if (result.missed) console.log('missed')
-
-  const html = await generateHtml()
-  res.send(html.replace('<!-- server -->', htmlApp))
+  if (context.url) {
+    console.log('redirect')
+    res.writeHead(302, {
+      Location: context.url
+    })
+    res.end()
+  } else {
+    const html = await generateHtml()
+    res.send(html.replace('<!-- server -->', htmlApp))
+  }
 })
 
 export default app
