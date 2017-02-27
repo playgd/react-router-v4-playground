639ce7c1931a3ba365edc4769a559abc6b5c747b
Fernando Daciuk <f.daciuk@gmail.com>
2017-02-26 22:36:45 -0300

    Add react router on app

--- server-side-rendering/src/app.js
+++ server-side-rendering/src/app.js
@@ -2,12 +2,26 @@
 
 import React from 'react'
 import Title from 'components/title'
+import { Route, Link } from 'react-router-dom'
 
 import 'css/style.css'
 
 const App = () => (
   <div>
     <Title>My App</Title>
+
+    <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+    </ul>
+
+    <Route exact path='/' render={({ match }) => (
+      <h1>Home!</h1>
+    )} />
+
+    <Route path='/about' render={({ match }) => (
+      <h1>About!</h1>
+    )} />
   </div>
 )
 
